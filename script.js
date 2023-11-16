5220665077f03ec660d6d13787d8760a50b83930
Tattoo1234 <147221184+Tattoo1234@users.noreply.github.com>
2023-11-16 15:36:23 -0800

    repaired JS to show proper function of button

--- script.js
+++ script.js
@@ -10,43 +10,47 @@ var toComplete = "";
 
 
 function generatePassword() {
-      var passwordString = ""; 
-      numberCharacters = window.prompt("Select Number of Characters") 
-        if (!numberCharacters || numberCharacters <8 || numberCharacters >128) {
-          alert("Entry must be between 8 and 128 Characters")
-          return;
-        }
-      
-       
-        
-        
-      var includeSpec = window.confirm("Include Special Characters?")
-      var includeNum = window.confirm("Include Numbers?")
-      var includeLow = window.confirm("Include Lowercase Characters?")
-      var includeUpp = window.confirm("Include Uppercase Characters?")
+  var passwordString = "";
+  numberCharacters = window.prompt("Select Number of Characters")
+  if (!numberCharacters || numberCharacters < 8 || numberCharacters > 128) {
+    alert("Entry must be between 8 and 128 Characters")
+    return;
+  }
 
-        if (includeSpec) {
-            toComplete = toComplete + specialCharacter}
-        if (includeNum) {
-            toComplete = toComplete + number}
-        if (includeLow) {
-            toComplete = toComplete + lowercase}
-        if (includeUpp) { 
-            toComplete = toComplete + uppercase}
 
-        //    else (includeSpec && includeNum && includeLow && includeUpp) {
-        //       alert("At least one character type needs to be selected")}
-        }
 
-      
-        
-      for(let i=0; i<numberCharacters; i++){
-      passwordString = passwordString + toComplete[Math.floor(Math.random() * toComplete.length)]; 
-      
-      
-      return passwordString
 
-      }
+  var includeSpec = window.confirm("Include Special Characters?")
+  var includeNum = window.confirm("Include Numbers?")
+  var includeLow = window.confirm("Include Lowercase Characters?")
+  var includeUpp = window.confirm("Include Uppercase Characters?")
+
+  if (includeSpec) {
+    toComplete = toComplete + specialCharacter
+  }
+  if (includeNum) {
+    toComplete = toComplete + number
+  }
+  if (includeLow) {
+    toComplete = toComplete + lowercase
+  }
+  if (includeUpp) {
+    toComplete = toComplete + uppercase
+  }
+
+  //    else (includeSpec && includeNum && includeLow && includeUpp) {
+  //       alert("At least one character type needs to be selected")}
+
+
+
+
+  for (let i = 0; i < numberCharacters; i++) {
+    passwordString = passwordString + toComplete[Math.floor(Math.random() * toComplete.length)];
+  }
+
+  return passwordString
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -56,8 +60,6 @@ function writePassword() {
   passwordText.value = password;
 
 
-
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-}
